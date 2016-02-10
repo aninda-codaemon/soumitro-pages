4d0485198db37ad89f836299c88c528b9c88061c
amazingBastard <dvazgird@beenverified.com>
2016-02-10 13:51:52 -0500

    updated tos and footer links

--- SOT-869/js/runner.js
+++ SOT-869/js/runner.js
@@ -1,5 +1,6 @@
-;(function ($) {
-  var trackNL = function (evtName, props) {
+;
+(function($) {
+  var trackNL = function(evtName, props) {
     if (typeof nolimit !== 'undefined' && nolimit.track) {
       if (props) {
         nolimit.track(evtName, props);
@@ -18,44 +19,44 @@
 
   /* Initialize cvv popover for Bootstrap 3 */
   $('.cvv-tip').popover({
-      container: 'body',
-      trigger: 'hover focus',
-      placement:'auto',
-      title:'<p><strong>What is a Security Code?</strong></p>',
-      html: true,
-      content: function () {
-          return '<p><strong>Visa, MasterCard, and Discover</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-visa.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Back of Card</strong><br>Three digits located on the right of the signature strip.</small></p></div></div><p><strong>American Express</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-amex.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Front of Card</strong><br>Four digits located on either the left or right side.</small></p></div>';
-      }
+    container: 'body',
+    trigger: 'hover focus',
+    placement: 'auto',
+    title: '<p><strong>What is a Security Code?</strong></p>',
+    html: true,
+    content: function() {
+      return '<p><strong>Visa, MasterCard, and Discover</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-visa.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Back of Card</strong><br>Three digits located on the right of the signature strip.</small></p></div></div><p><strong>American Express</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-amex.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Front of Card</strong><br>Four digits located on either the left or right side.</small></p></div>';
+    }
   });
   /* Initialize security popover for Bootstrap 3 */
   $('.secure').popover({
-      container: 'body',
-      trigger: 'hover focus',
-      placement:'auto',
-      html: true,
-      content: function () {
-          return '<p><span class="glyphicon glyphicon-lock"></span> BeenVerified deploys the latest and greatest strategies, including Secure 256-bit SSL technology, to keep your personal information and payment data safe from unauthorized 3rd parties.</p>';
-      }
+    container: 'body',
+    trigger: 'hover focus',
+    placement: 'auto',
+    html: true,
+    content: function() {
+      return '<p><span class="glyphicon glyphicon-lock"></span> BeenVerified deploys the latest and greatest strategies, including Secure 256-bit SSL technology, to keep your personal information and payment data safe from unauthorized 3rd parties.</p>';
+    }
   });
 
   var $selectionWrap = $('.selection-wrap');
 
   // Update terms & conditions based on selected plan
-  var updatePlanLegal = function ($selectionSelected) {
+  var updatePlanLegal = function($selectionSelected) {
 
     var $planPrice = $selectionSelected.find(".plan-price");
 
     var totalPrice = $planPrice.data('plan-price'),
-        type = $planPrice.data('plan-type'),
-        monthlyPrice = $planPrice.data('monthly-price'),
-        termLength = $planPrice.data('term-length');
+      type = $planPrice.data('plan-type'),
+      monthlyPrice = $planPrice.data('monthly-price'),
+      termLength = $planPrice.data('term-length');
 
 
     var $priceHTML = $('#legal-price'),
-        $typeHTML = $('#legal-type'),
-        $summaryTermHTML = $('#summary-term'),
-        $summaryMonthPriceHTML = $('#summary-month-price'),
-        $summaryTotalPriceHTML = $('.summary-total-price');
+      $typeHTML = $('#legal-type'),
+      $summaryTermHTML = $('#summary-term'),
+      $summaryMonthPriceHTML = $('#summary-month-price'),
+      $summaryTotalPriceHTML = $('.summary-total-price');
 
     $priceHTML.html(totalPrice);
     $summaryTotalPriceHTML.html(totalPrice);
@@ -70,7 +71,7 @@
   var highlightSelection = function() {
     $selectionWrap.removeClass('selection-selected');
     $(this).addClass('selection-selected');
-  $(this).find("input[type=radio].plan_name_radio").prop("checked", true);
+    $(this).find("input[type=radio].plan_name_radio").prop("checked", true);
     updatePlanLegal($(this));
   };
 
@@ -78,17 +79,17 @@
 
   // Secure Credit Card lock animation
   var $secureLockIcon = $('#secure-lock'),
-      $ccInput = $('#credit_card_card_number'),
-      $secureText = $('#secure-text');
+    $ccInput = $('#credit_card_card_number'),
+    $secureText = $('#secure-text');
 
-      // $secureText.hide();
+  // $secureText.hide();
 
-  $ccInput.on('focus' , function () {
-      $secureLockIcon.addClass('secure-this');
-      $secureText.fadeIn();
+  $ccInput.on('focus', function() {
+    $secureLockIcon.addClass('secure-this');
+    $secureText.fadeIn();
   });
 
-  $ccInput.on('change blur' , function () {
+  $ccInput.on('change blur', function() {
     var ccInputLength = $ccInput.val().length;
 
     if (!ccInputLength) {
@@ -98,10 +99,11 @@
   });
 
   /* Set current year inside footer */
-  ;(function ($) {
+  ;
+  (function($) {
     var currentDate = new Date(),
-        currentYear = currentDate.getFullYear(),
-        $currentYear = $('.current-year');
+      currentYear = currentDate.getFullYear(),
+      $currentYear = $('.current-year');
 
     $currentYear.html(currentYear);
   }(jQuery));
@@ -109,53 +111,53 @@
   // Clicking Credit Card logo will focus on input field
   var $ccAccept = $('.cc-accept');
 
-  $ccAccept.on('click' , function () {
+  $ccAccept.on('click', function() {
     $ccInput.focus();
   });
 
-  var verifySeal = function () {
-    $(".verify-seal").on("click", function () {
-      var left  = ($(window).width()/2)-(900/2),
-          top   = ($(window).height()/2)-(600/2),
-          popup = window.open ("https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&dn=www.beenverified.com&lang=en", "popup", "width=900, height=600, top="+top+", left="+left);
+  var verifySeal = function() {
+    $(".verify-seal").on("click", function() {
+      var left = ($(window).width() / 2) - (900 / 2),
+        top = ($(window).height() / 2) - (600 / 2),
+        popup = window.open("https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&dn=www.beenverified.com&lang=en", "popup", "width=900, height=600, top=" + top + ", left=" + left);
     });
   };
 
-  var playVideo = function () {
-    $('#youtube').click(function(){
-        var video = '<iframe src="'+ $(this).attr('data-video') +'"></iframe>';
-        $(this).replaceWith(video);
+  var playVideo = function() {
+    $('#youtube').click(function() {
+      var video = '<iframe src="' + $(this).attr('data-video') + '"></iframe>';
+      $(this).replaceWith(video);
     });
   };
 
-  var reportHeap = function (evt, opt) {
+  var reportHeap = function(evt, opt) {
     if (typeof window.heap !== "undefined" && heap.track) {
       heap.track(evt, opt);
     }
   };
 
   var $bounceBackBtn = $('#iModal-back'),
-      $bounceExitBtn = $('#iModal-exit'),
-      $goToNextPage  = $('#show-dollar-trial'),
-      $iOSModal      = $('#iModal'),
-      $iOSModalTrial = $('#iModal-trial'),
-      $iModalX       = $("#imodal-x");
+    $bounceExitBtn = $('#iModal-exit'),
+    $goToNextPage = $('#show-dollar-trial'),
+    $iOSModal = $('#iModal'),
+    $iOSModalTrial = $('#iModal-trial'),
+    $iModalX = $("#imodal-x");
 
-  var bounceBack = function () {
+  var bounceBack = function() {
     trackNL('onBack Modal - Rejected');
     //window.location.href = $("body").data("search-page");
   };
 
-  var bounceExit = function () {
+  var bounceExit = function() {
     $iOSModal.modal('hide');
-    window.setTimeout(function () {
-        $iOSModalTrial.modal('show');
+    window.setTimeout(function() {
+      $iOSModalTrial.modal('show');
     }, 300);
   };
 
-  $bounceBackBtn.on('click' , bounceBack);
-  $bounceExitBtn.on('click' , bounceExit);
-  $goToNextPage.on('click', function () {
+  $bounceBackBtn.on('click', bounceBack);
+  $bounceExitBtn.on('click', bounceExit);
+  $goToNextPage.on('click', function() {
     trackNL('onBack Modal - Accepted');
     window.location.href = $("body").data("next-page");
   });
@@ -165,28 +167,28 @@
   });
 
 
-  var initDownsells = function () {
+  var initDownsells = function() {
 
     var VWO_CHECK_INTERVAL = 3000,
-        CHECK_TIMEOUT = 5000,
-        timeElapsed = 1000;
+      CHECK_TIMEOUT = 5000,
+      timeElapsed = 1000;
 
-    var activateDownsells = function () {
+    var activateDownsells = function() {
       if (typeof downsell !== "undefined" && typeof downsell.init === "function") {
         downsell.init({
           onBack: {
             elem: "#iModal-trial",
-            cb: function () {}
+            cb: function() {}
           }
         });
       }
     };
 
     var vwoIntervalId,
-        vwoExists = typeof _vwo_code !== "undefined" && typeof _vwo_code.finished === 'function';
+      vwoExists = typeof _vwo_code !== "undefined" && typeof _vwo_code.finished === 'function';
 
     if (vwoExists) {
-      vwoIntervalId = window.setInterval(function () {
+      vwoIntervalId = window.setInterval(function() {
         timeElapsed += VWO_CHECK_INTERVAL;
         if (timeElapsed > CHECK_TIMEOUT || _vwo_code.finished()) {
           window.clearInterval(vwoIntervalId);
@@ -204,7 +206,7 @@
     //amplify.store("lastVisit", Date.now());
   }
 
-  var initialize = function () {
+  var initialize = function() {
     verifySeal();
     playVideo();
     setLastVisit();
@@ -212,7 +214,7 @@
     var selectedPlan = $(".selection-selected");
     updatePlanLegal(selectedPlan);
 
-  initDownsells();
+    initDownsells();
   };
 
   initialize();
