66acae1c5b664f7f8dd8a128951f184eef1d7a81
amazingBastard <dvazgird@beenverified.com>
2016-02-26 11:19:23 -0500

    teaser data for private mode wip

--- SOT-869/js/runner.js
+++ SOT-869/js/runner.js
@@ -31,159 +31,159 @@
     var getTeaserData = function() {
 
       var baseUrl = "//www.beenverified.com/hk/dd/teaser/person?exporttype=jsonp",
-        url = baseUrl + '&bvid=' + query.bvid,
-        xhrData = $.ajax({
-          url: url,
-          dataType: 'jsonp',
-          jsonpCallback: 'parseResults',
-          beforeSend: function(xhr){
-            console.log(xhr);
-            $('#page-loader').show();
-          },
-          success: function(result) {
-            console.log(result);
-            $('#page-loader').hide();
-          }
-        });
+        url = baseUrl + '&bvid=' + query.bvid;
 
-      $.when(xhrData).done(function(result) {
-        trackNL('Queried Person Teaser Data Called');
+      $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        jsonpCallback: 'parseResults',
+        beforeSend: function(xhr) {
+          $('#page-loader').show();
 
-        console.log(result);
+          // @TODO: need to conditionlly load the data through handlebars?
+        },
+        success: function(result) {
+          trackNL('Queried Person Teaser Data Called');
 
-        var names = $.map(result.names, function(item) {
-          return {
-            'First': item.parts.first_name,
-            'Last': item.parts.last_name,
-            'Middle': item.parts.middle_name,
-            'Prefix': item.parts.suffix
+          var names = $.map(result.names, function(item) {
+            return {
+              'First': item.parts.first_name,
+              'Last': item.parts.last_name,
+              'Middle': item.parts.middle_name,
+              'Prefix': item.parts.suffix
+            };
+          });
+
+          var relatives = $.map(result.connections.relatives, function(item) {
+            return {
+              'First': item.names[0].parts.first_name,
+              'Last': item.names[0].parts.last_name,
+              'bvid': item.id
+            };
+          });
+
+          var ages = $.map(result.ages, function(item) {
+            // @TODO: loop and send age that is defined
+            return item;
+          });
+
+          var addresses = $.map(result.addresses, function(item) {
+            return {
+              'City': item.parts.city,
+              'State': item.parts.state,
+              'Zip4': item.parts.zip4,
+              'Zip5': item.parts.zip
+            };
+          });
+
+          var phoneNumbers = $.map(result.phones, function(item) {
+            return item.number.formatPhone();
+          });
+
+          var emailAddresses = $.map(result.emails, function(item) {
+            return item.email_address.formatEmail().toLowerCase();
+          });
+
+          var socialNetworks = $.map(result.social, function(item) {
+            return item.type.nameize();
+          });
+
+          // Data elements to display - Waterfall controlled here
+
+          var extraTeaserData = [{
+            'type': 'criminal',
+            'name': 'Criminal or Traffic*',
+            'single': 'Criminal or Traffic*',
+            'style': ' crim-box',
+            'weight': 3,
+            'showIfEmpty': 0,
+            'count': result.courts.criminal.length
+          }, {
+            'type': 'emails',
+            'name': 'Email Addresses',
+            'single': 'Email Address',
+            'style': '',
+            'weight': 2,
+            'showIfEmpty': 0,
+            'count': result.emails.length,
+            'emailAddress': emailAddresses
+          }, {
+            'type': 'phones',
+            'name': 'Phone Numbers',
+            'single': 'Phone Number',
+            'style': ' phone-box',
+            'weight': 1,
+            'showIfEmpty': 0,
+            'count': result.phones.length,
+            'phoneNumber': phoneNumbers
+          }, {
+            'type': 'social',
+            'name': 'Social Media Profiles',
+            'single': 'Social Media Profile',
+            'style': ' social-box',
+            'weight': 1,
+            'showIfEmpty': 0,
+            'count': result.social.length,
+            'socialNetwork': socialNetworks
+          }];
+
+          // Booleans for templating & reporting
+
+          var hasCriminal = _.some(extraTeaserData, function(item) {
+            return item.type === 'criminal' && item.count > 0;
+          });
+          var hasPhone = _.some(extraTeaserData, function(item) {
+            return item.type === 'phones' && item.count > 0;
+          });
+          var hasEmail = _.some(extraTeaserData, function(item) {
+            return item.type === 'emails' && item.count > 0;
+          });
+          var hasSocial = _.some(extraTeaserData, function(item) {
+            return item.type === 'social' && item.count > 0;
+          });
+          var hasProperty = _.some(extraTeaserData, function(item) {
+            return (item.type === 'addresses' && item.count > 0) || (item.type === 'neighbors' && item.count > 0);
+          });
+
+          // Force singular name here rather than in template... meh
+
+          extraTeaserData = _.forEach(extraTeaserData, function(item, key) {
+            if (item.count === 1) {
+              item.name = item.single;
+            }
+          });
+
+          // Scrub data for display
+
+          _.remove(extraTeaserData, function(item) {
+            return item.showIfEmpty === 0 && item.count === 0;
+          });
+
+          // Store data
+
+          var teaserData = {
+            Addresses: {
+              Address: addresses
+            },
+            Names: {
+              Name: names
+            },
+            Relatives: {
+              Relative: relatives
+            },
+            age: ages[0],
+            recordCount: ($.type(result) !== 'array' ? 1 : 0),
+            extraData: extraTeaserData,
+            hasCriminal: hasCriminal,
+            hasPhone: hasPhone,
+            hasEmail: hasEmail,
+            hasSocial: hasSocial
           };
-        });
 
-        var relatives = $.map(result.connections.relatives, function(item) {
-          return {
-            'First': item.names[0].parts.first_name,
-            'Last': item.names[0].parts.last_name,
-            'bvid': item.id
-          };
-        });
+          amplify.store('extraTeaserData', teaserData);
 
-        var ages = $.map(result.ages, function(item) {
-          // @TODO: loop and send age that is defined
-          return item;
-        });
-
-        var addresses = $.map(result.addresses, function(item) {
-          return {
-            'City': item.parts.city,
-            'State': item.parts.state,
-            'Zip4': item.parts.zip4,
-            'Zip5': item.parts.zip
-          };
-        });
-
-        var phoneNumbers = $.map(result.phones, function(item) {
-          return item.number.formatPhone();
-        });
-
-        var emailAddresses = $.map(result.emails, function(item) {
-          return item.email_address.formatEmail().toLowerCase();
-        });
-
-        var socialNetworks = $.map(result.social, function(item) {
-          return item.type.nameize();
-        });
-
-        // Data elements to display - Waterfall controlled here
-        var extraTeaserData = [{
-          'type': 'criminal',
-          'name': 'Criminal or Traffic*',
-          'single': 'Criminal or Traffic*',
-          'style': ' crim-box',
-          'weight': 3,
-          'showIfEmpty': 0,
-          'count': result.courts.criminal.length
-        }, {
-          'type': 'emails',
-          'name': 'Email Addresses',
-          'single': 'Email Address',
-          'style': '',
-          'weight': 2,
-          'showIfEmpty': 0,
-          'count': result.emails.length,
-          'emailAddress': emailAddresses
-        }, {
-          'type': 'phones',
-          'name': 'Phone Numbers',
-          'single': 'Phone Number',
-          'style': ' phone-box',
-          'weight': 1,
-          'showIfEmpty': 0,
-          'count': result.phones.length,
-          'phoneNumber': phoneNumbers
-        }, {
-          'type': 'social',
-          'name': 'Social Media Profiles',
-          'single': 'Social Media Profile',
-          'style': ' social-box',
-          'weight': 1,
-          'showIfEmpty': 0,
-          'count': result.social.length,
-          'socialNetwork': socialNetworks
-        }];
-
-        // Booleans for templating & reporting
-        var hasCriminal = _.some(extraTeaserData, function(item) {
-          return item.type === 'criminal' && item.count > 0;
-        });
-        var hasPhone = _.some(extraTeaserData, function(item) {
-          return item.type === 'phones' && item.count > 0;
-        });
-        var hasEmail = _.some(extraTeaserData, function(item) {
-          return item.type === 'emails' && item.count > 0;
-        });
-        var hasSocial = _.some(extraTeaserData, function(item) {
-          return item.type === 'social' && item.count > 0;
-        });
-        var hasProperty = _.some(extraTeaserData, function(item) {
-          return (item.type === 'addresses' && item.count > 0) || (item.type === 'neighbors' && item.count > 0);
-        });
-
-        // Force singular name here rather than in template... meh
-        extraTeaserData = _.forEach(extraTeaserData, function(item, key) {
-          if (item.count === 1) {
-            item.name = item.single;
-          }
-        });
-
-        // Scrub data for display
-        _.remove(extraTeaserData, function(item) {
-          return item.showIfEmpty === 0 && item.count === 0;
-        });
-
-        // Store data
-        var teaserData = {
-          Addresses: {
-            Address: addresses
-          },
-          Names: {
-            Name: names
-          },
-          Relatives: {
-            Relative: relatives
-          },
-          age: ages[0],
-          recordCount: ($.type(result) !== 'array' ? 1 : 0),
-          extraData: extraTeaserData,
-          hasCriminal: hasCriminal,
-          hasPhone: hasPhone,
-          hasEmail: hasEmail,
-          hasSocial: hasSocial
-        };
-
-        console.log(teaserData);
-
-        amplify.store('extraTeaserData', teaserData);
+          $('#page-loader').hide();
+        }
       });
     };
 
