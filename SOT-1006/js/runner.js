d2b6cc0b396e6e5333e0366b9e9348ed1bc18c5e
amazingBastard <dvazgird@beenverified.com>
2016-05-12 13:01:43 -0400

    regex validation method wip

--- SOT-1006/js/runner.js
+++ SOT-1006/js/runner.js
@@ -287,6 +287,15 @@
 
     /* Validations */
 
+    $.validator.addMethod(
+        'regex',
+        function(value, element, regexp) {
+            var re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your input."
+    );
+
     var searchRules = {
         rules: {
             fn: "required",
