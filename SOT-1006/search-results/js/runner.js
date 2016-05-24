dbe92162a46de53fdb767805a18e260050f08ce0
amazingBastard <dvazgird@beenverified.com>
2016-05-24 14:46:51 -0400

    updated parseMiddleInitial func to seperate second first name if one is inputed

--- SOT-1006/search-results/js/runner.js
+++ SOT-1006/search-results/js/runner.js
@@ -255,7 +255,7 @@
         $("#data-panel").hide();
         $("#results-wrapper-searching").fadeIn();
     };
-  
+
   	var removeDiacritics = function(str) {
       var defaultDiacriticsRemovalMap = [
         {'base':'A', 'letters':/[\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u00C4\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F]/g},
@@ -362,9 +362,10 @@
         if (!data.mi || data.mi.length === 0) {
           data.mi = parsed_mi[1];
         }
-        data.fn = data.fn.replace(/\s[A-Za-z]$/, '');
+        data.fn = data.fn.replace(/\s[A-Za-z]$/, '').replace(/\s+/g, '');
+      } else {
+        data.fn = data.fn.replace(/\s+/g, '');
       }
-
       return data;
     }
 
