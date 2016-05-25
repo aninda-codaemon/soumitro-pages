8af12341270eea815c41ec16fda2da19b0b9eecd
amazingBastard <dvazgird@beenverified.com>
2016-05-25 16:04:41 -0400

    SOT-1266 init

---
+++ SOT-1266/js/runner.js
@@ -0,0 +1,40 @@
+(function() {
+  // NoLimit/Heap init
+  var trackNL = function(evtName, props) {
+    if (typeof nolimit !== 'undefined' && nolimit.track) {
+      if (props) {
+        nolimit.track(evtName, props);
+      } else {
+        nolimit.track(evtName);
+      }
+    }
+    if (typeof heap !== 'undefined' && heap.track) {
+      if (props) {
+        heap.track(evtName, props);
+      } else {
+        heap.track(evtName);
+      }
+    }
+  };
+
+  // Test for Safari private browsing mode
+  try {
+    localStorage.test = 2;
+  } catch (e) {
+    trackNL('Safari Private Browsing');
+  }
+
+  // Set current year inside footer
+  ;(function ($) {
+    var currentDate = new Date(),
+        currentYear = currentDate.getFullYear(),
+        $currentYear = $('.current-year');
+    $currentYear.html(currentYear);
+  }(jQuery));
+
+  var init = function() {
+    $('.focus-on').focus();
+  };
+
+  init();
+}());
