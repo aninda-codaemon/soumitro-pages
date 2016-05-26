b797cb34053c7c304bcd1c405ca28355b7fd15b9
amazingBastard <dvazgird@beenverified.com>
2016-05-26 11:49:58 -0400

    fixed css issues

--- SOT-1266/js/subscribe.js
+++ SOT-1266/js/subscribe.js
@@ -293,8 +293,8 @@
           required: "Please confirm your email.",
           equalTo: "Please enter the same email address as above."
       },
-      tos2: {
-          required: "You will need to agree in order to use our services."
+      "account[tos]": {
+          required: "Please agree to our terms before proceeding."
       },
       "user[email]": "Please enter a valid email address."
     }
