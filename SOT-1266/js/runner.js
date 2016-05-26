6610dac66a22a8c52de77bc99461674b0db4a0d6
amazingBastard <dvazgird@beenverified.com>
2016-05-26 12:13:33 -0400

    updates to runner - styled popovers

--- SOT-1266/js/runner.js
+++ SOT-1266/js/runner.js
@@ -1,4 +1,4 @@
-(function() {
+;(function ($) {
   // NoLimit/Heap init
   var trackNL = function(evtName, props) {
     if (typeof nolimit !== 'undefined' && nolimit.track) {
@@ -17,12 +17,47 @@
     }
   };
 
+  var reportHeap = function (evt, opt) {
+    if (typeof window.heap !== "undefined" && heap.track) {
+      heap.track(evt, opt);
+    }
+  };
+
   // Test for Safari private browsing mode
   try {
     localStorage.test = 2;
   } catch (e) {
     trackNL('Safari Private Browsing');
   }
+
+  // Make sure the first plan is selected - IE10 fix
+  var $planRows = $("input[name=subscription_plan_name]");
+  if ($planRows.length > 0) {
+    $planRows[0].click();
+  }
+
+  // Initialize cvv popover for Bootstrap 3
+  $('.img-cards').popover({
+      container: 'body',
+      trigger: 'hover focus',
+      placement:'auto',
+      title:'<h6 class="title">What is a Security Code?</h6>',
+      html: true,
+      content: function () {
+        return '<p class="sub-title">Visa, MasterCard, and Discover</p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-visa.png"></div><div class="col-xs-6"><p class="popover-text"><strong class="text-break">Back of Card</strong>Three digits located on the right of the signature strip.</p></div></div><p class="sub-title">American Express</p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-amex.png"></div><div class="col-xs-6"><p class="popover-text"><strong class="text-break">Front of Card</strong>Four digits located on either the left or right side.</p></div>';
+      }
+  });
+
+  // Initialize security popover for Bootstrap 3
+  $('.img-secure').popover({
+      container: 'body',
+      trigger: 'hover focus',
+      placement:'auto',
+      html: true,
+      content: function () {
+        return '<p class="popover-text"><i class="fa fa-lock icon"></i> BeenVerified deploys the latest and greatest strategies, including Secure 256-bit SSL technology, to keep your personal information and payment data safe from unauthorized 3rd parties.</p>';
+      }
+  });
 
   // Set current year inside footer
   ;(function ($) {
@@ -37,4 +72,4 @@
   };
 
   init();
-}());
+}(jQuery));
