71eb2e65cffc03947f4a3537185b9ecce41e80b5
amazingBastard <dvazgird@beenverified.com>
2016-06-20 16:55:23 -0400

    profile links added

--- SOT-1270/staging/ci/application/views/home.php
+++ SOT-1270/staging/ci/application/views/home.php
@@ -29,47 +29,47 @@ $this->load->view('parts/header', $info);
         <h3>Recently Updated Profiles</h3>
           <ul>
             <li>
-                <a href="#">Amber B Taylor</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/meredith-berger-10540690/">Meredith Berger</a>
+                  <p>66 Years Old</p>
+                  <p>Florham Park, NJ</p>
               </li>
             <li>
-                <a href="#">David L Ruzicka</a>
-                  <p>62 Years Old</p>
+                <a href="/people/david-l-ruzicka-113028513/">David L Ruzicka</a>
+                  <p>46 Years Old</p>
                   <p>Humboldt, IA</p>
               </li>
             <li>
-                <a href="#">Kimberly C Eads</a>
-                  <p>62 Years Old</p>
+                <a href="/people/kimberly-eads/">Kimberly C Eads</a>
+                  <p>43 Years Old</p>
                   <p>Kansas City, MO</p>
               </li>
             <li>
-                <a href="#">Carma Taylor Davis</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/carma-taylor-davis-32245129/">Carma Taylor Davis</a>
+                  <p>60 Years Old</p>
+                  <p>Denver, CO</p>
               </li>
             <li>
-                <a href="#">Amber B Taylor</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/joanne-mcnulty-87415616/">Joanne McNulty</a>
+                  <p>64 Years Old</p>
+                  <p>Maple Shade, NJ</p>
               </li>
             <li>
-                <a href="#">Amber B Taylor</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/craig-weinstein/">Craig A Weinstein</a>
+                  <p>45 Years Old</p>
+                  <p>Maple Valley, WA</p>
               </li>
             <li>
-                <a href="#">Amber B Taylor</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/lisa-dimarco/">Lisa Lynn Dimarco</a>
+                  <p>57 Years Old</p>
+                  <p>North Windham, CT</p>
               </li>
             <li>
-                <a href="#">Amber B Taylor</a>
-                  <p>62 Years Old</p>
-                  <p>Middle Town, AR</p>
+                <a href="/people/barry-field/">Barry E Field</a>
+                  <p>70 Years Old</p>
+                  <p>Bronx, NY</p>
               </li>
           </ul>
-          <a class="view-more">View More Profiles</a>
+          <a class="view-more" href="/people/">View More Profiles</a>
       </div>
 </section>
 <!-- End Profiles -->
