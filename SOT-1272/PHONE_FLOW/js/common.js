9e70536c886c92ad929c87eccf0a4256e1970141
amazingBastard <dvazgird@beenverified.com>
2016-08-11 16:30:52 -0400

    deref old map code

--- SOT-1272/PHONE_FLOW/js/common.js
+++ SOT-1272/PHONE_FLOW/js/common.js
@@ -4,29 +4,28 @@
 
   var url = 'https://www.beenverified.com/hk/dd/free/phoneinfo?&exporttype=jsonp';
   var subCheckUrl = 'https://www.beenverified.com/hk/dd/premium/phone/reverseteaser?exporttype=jsonp';
-  var mapNum = 60352;
-  var apiKey = '26a01f91e0ce4bf99a5e773b7745b649';
-  var tilesUrl = 'https://ssl_tiles.cloudmade.com/' + apiKey + '/' + mapNum + '/256/{z}/{x}/{y}.png';
+  // var mapNum = 60352;
+  // var apiKey = '26a01f91e0ce4bf99a5e773b7745b649';
+  // var tilesUrl = 'https://ssl_tiles.cloudmade.com/' + apiKey + '/' + mapNum + '/256/{z}/{x}/{y}.png';
   var landingUrl = "https://www.beenverified.com";
   var subscribeUrl = "https://www.beenverified.com/subscribe/reverse_phone_lookup";
   var blueCheckUrl = 'http://frcdn.beenverified.com/assets/img/d1c73bfcb2bc605825980a9e2095e758.png';
-  //var blueCheckUrl = 'https://cdn1.beenverified.com/srg/Reverse-Phone/landing/img/blueCheck_32.png';
 
   var mapAccessToken = 'pk.eyJ1IjoiYmVlbnZlcmlmaWVkIiwiYSI6InBLR3UwVG8ifQ.tCCuBmKzRqNMGKIY2C1YOw'; // mapbox access token
   var mapStyle = 'mapbox://styles/beenverified/cilnvi7je005t9nkujb0fpxue'; // link to number guru mapbox style
   var mapContainer = 'map'; // id of map container
 
-  var mapOpts = {
-    dragging: false,
-    touchZoom: false,
-    scrollWheelZoom: false,
-    doubleClickZoom: false,
-    boxZoom: false,
-    keyboard: false,
-    zoomControl: false,
-    startZoom: 2,
-    attributionControl: false
-  };
+  // var mapOpts = {
+  //   dragging: false,
+  //   touchZoom: false,
+  //   scrollWheelZoom: false,
+  //   doubleClickZoom: false,
+  //   boxZoom: false,
+  //   keyboard: false,
+  //   zoomControl: false,
+  //   startZoom: 2,
+  //   attributionControl: false
+  // };
 
   // map options
   var mapOptions = {
@@ -48,10 +47,10 @@
     token: mapAccessToken
   };
 
-  var leaflet = {
-    mapOpts: mapOpts,
-    tilesUrl: tilesUrl
-  };
+  // var leaflet = {
+  //   mapOpts: mapOpts,
+  //   tilesUrl: tilesUrl
+  // };
 
   var config = {
     searchData: 'bv_searchData',
@@ -65,7 +64,7 @@
 
   window.bvRPL = {
     config: config,
-    leaflet: leaflet,
+    // leaflet: leaflet,
     mapbox: mapbox
   };
 
