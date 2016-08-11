fdc6f85e9b4c849438a51cdc8b38b4b190692024
amazingBastard <dvazgird@beenverified.com>
2016-08-11 17:09:09 -0400

    map animation refactoring wip

--- SOT-1272/PHONE_FLOW/js/map_animation.js
+++ SOT-1272/PHONE_FLOW/js/map_animation.js
@@ -1,95 +1,93 @@
-(function ($, L, AnimationFrame) {
-
-  /* Patches window.requestAnimationFrame for cross-browser support. */
-  AnimationFrame.shim();
-
+// (function ($, L, AnimationFrame) {
+(function ($) {
   var map;
 
-  var circleOpts = {
-    color: '#b10810',
-    opacity: 1,
-    fillColor: '#b10810',
-    fillOpacity: 0.25,
-    weight: 2,
-    size: 0.5,
-    clickable: false,
-  };
+  // var circleOpts = {
+  //   color: '#b10810',
+  //   opacity: 1,
+  //   fillColor: '#b10810',
+  //   fillOpacity: 0.25,
+  //   weight: 2,
+  //   size: 0.5,
+  //   clickable: false,
+  // };
 
-  var circle = L.circleMarker([38.505191, -97.734375], circleOpts);
+  // var circle = L.circleMarker([38.505191, -97.734375], circleOpts);
 
-  var resetCircleRadius = function () {
-    circleOpts.radius = ($(window).width() * 0.40) / 2;
-    circle.setRadius(circleOpts.radius);
-  };
+  // var resetCircleRadius = function () {
+  //   circleOpts.radius = ($(window).width() * 0.40) / 2;
+  //   circle.setRadius(circleOpts.radius);
+  // };
+  //
+  // resetCircleRadius();
+  //
+  // var circleRadius = circleOpts.radius;
+  // circle.setRadius(circleRadius);
+  // var currZoomLevel = 5;
+  // var circleMinRadius = 10;
+  // var circleStep = 2.5;
+  // @TODO: get actual phone location
+  var targetLocation = [-73.97, 40.73];
 
-  resetCircleRadius();
+  // var animateCircleRadius = function () {
+  //   if (currCircleRadius < circleMinRadius) {
+  //     window.cancelAnimationFrame(animateCircleRadiusId);
+  //     circle.setRadius(currCircleRadius);
+  //     map.setView(targetLocation, currZoomLevel, {animate: true});
+  //     nextTask();
+  //   } else {
+  //     currCircleRadius -= circleStep;
+  //     circle.setRadius(currCircleRadius);
+  //     animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
+  //   }
+  // };
+  //
+  // var runCircleAnimation = function (waitTime) {
+  //   circle.setRadius(circleOpts.radius);
+  //   window.setTimeout(function () {
+  //     currCircleRadius = circleOpts.radius;
+  //     animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
+  //   }, waitTime);
+  // };
+  //
+  // var transitionDuration = 500;
 
-  var circleRadius = circleOpts.radius;
-  circle.setRadius(circleRadius);
-  var currZoomLevel = 5;
-  var circleMinRadius = 10;
-  var circleStep = 2.5;
-  var targetLocation = [38.505191, -97.734375];
+  // var tasks = [
+  //   function () {
+  //     currZoomLevel = 7;
+  //     runCircleAnimation(transitionDuration);
+  //   },
+  //   function () {
+  //     currZoomLevel = 9;
+  //     runCircleAnimation(transitionDuration);
+  //   },
+  //   function () {
+  //     currZoomLevel = 12;
+  //     runCircleAnimation(transitionDuration);
+  //   }
+  // ];
 
-  var animateCircleRadius = function () {
-    if (currCircleRadius < circleMinRadius) {
-      window.cancelAnimationFrame(animateCircleRadiusId);
-      circle.setRadius(currCircleRadius);
-      map.setView(targetLocation, currZoomLevel, {animate: true});
-      nextTask();
-    } else {
-      currCircleRadius -= circleStep;
-      circle.setRadius(currCircleRadius);
-      animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
-    }
-  };
+  // var finishedCb = function () {};
+  //
+  // var onCompletion = function () {
+  //   circle.setRadius(circleOpts.radius);
+  //   window.setTimeout(function () {
+  //     map.removeLayer(circle);
+  //     finishedCb();
+  //   }, 1200);
+  // };
 
-  var runCircleAnimation = function (waitTime) {
-    circle.setRadius(circleOpts.radius);
-    window.setTimeout(function () {
-      currCircleRadius = circleOpts.radius;
-      animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
-    }, waitTime);
-  };
+  // var currTask = -1;
+  // var maxTasks = tasks.length - 1;
 
-  var transitionDuration = 500;
-
-  var tasks = [
-    function () {
-      currZoomLevel = 7;
-      runCircleAnimation(transitionDuration);
-    },
-    function () {
-      currZoomLevel = 9;
-      runCircleAnimation(transitionDuration);
-    },
-    function () {
-      currZoomLevel = 12;
-      runCircleAnimation(transitionDuration);
-    }
-  ];
-
-  var finishedCb = function () {};
-
-  var onCompletion = function () {
-    circle.setRadius(circleOpts.radius);
-    window.setTimeout(function () {
-      map.removeLayer(circle);
-      finishedCb();
-    }, 1200);
-  };
-
-  var currTask = -1;
-  var maxTasks = tasks.length - 1;
-
-  var nextTask = function () {
-    currTask += 1;
-    if (currTask <= maxTasks) {
-    tasks[currTask]();
-    } else {
-      onCompletion();
-    }
-  };
+  // var nextTask = function () {
+  //   currTask += 1;
+  //   if (currTask <= maxTasks) {
+  //   tasks[currTask]();
+  //   } else {
+  //     onCompletion();
+  //   }
+  // };
 
   // var phoneLocation = {
   //   center: [-73.97, 40.73],
@@ -111,7 +109,7 @@
 
   var run = function (mapInst, location, delay, cb) {
     map = mapInst;
-    finishedCb = cb;
+    // finishedCb = cb;
     $(".initial_modal").fadeOut();
     var $locating = $('.locating');
     $locating.show();
@@ -128,9 +126,9 @@
           speed: 0.25, // speed of fly animation
           curve: 0.75 // speed of zoom animation
       });
-      circle.setLatLng(targetLocation);
-      circle.addTo(map);
-      nextTask();
+      // circle.setLatLng(targetLocation);
+      // circle.addTo(map);
+      // nextTask();
     }, delay);
   };
 
@@ -138,4 +136,5 @@
     run: run
   };
 
-}(jQuery, L, AnimationFrame));
+// }(jQuery, L, AnimationFrame));
+}(jQuery));
