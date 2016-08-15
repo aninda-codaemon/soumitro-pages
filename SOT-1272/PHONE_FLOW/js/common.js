5e28c5821a4970028297370bbed7d9ee05767d34
amazingBastard <dvazgird@beenverified.com>
2016-08-15 12:04:21 -0400

    cleaned up code for new number guru map

--- SOT-1272/PHONE_FLOW/js/common.js
+++ SOT-1272/PHONE_FLOW/js/common.js
@@ -1,31 +1,12 @@
 (function () {
-
-  // @TODO: remove unused code once new code is tested
-
   var url = 'https://www.beenverified.com/hk/dd/free/phoneinfo?&exporttype=jsonp';
   var subCheckUrl = 'https://www.beenverified.com/hk/dd/premium/phone/reverseteaser?exporttype=jsonp';
-  // var mapNum = 60352;
-  // var apiKey = '26a01f91e0ce4bf99a5e773b7745b649';
-  // var tilesUrl = 'https://ssl_tiles.cloudmade.com/' + apiKey + '/' + mapNum + '/256/{z}/{x}/{y}.png';
   var landingUrl = "https://www.beenverified.com";
   var subscribeUrl = "https://www.beenverified.com/subscribe/reverse_phone_lookup";
   var blueCheckUrl = 'http://frcdn.beenverified.com/assets/img/d1c73bfcb2bc605825980a9e2095e758.png';
-
   var mapAccessToken = 'pk.eyJ1IjoiYmVlbnZlcmlmaWVkIiwiYSI6InBLR3UwVG8ifQ.tCCuBmKzRqNMGKIY2C1YOw'; // mapbox access token
   var mapStyle = 'mapbox://styles/beenverified/cilnvi7je005t9nkujb0fpxue'; // link to number guru mapbox style
   var mapContainer = 'map'; // id of map container
-
-  // var mapOpts = {
-  //   dragging: false,
-  //   touchZoom: false,
-  //   scrollWheelZoom: false,
-  //   doubleClickZoom: false,
-  //   boxZoom: false,
-  //   keyboard: false,
-  //   zoomControl: false,
-  //   startZoom: 2,
-  //   attributionControl: false
-  // };
 
   // map options
   var mapOptions = {
@@ -34,7 +15,7 @@
     center: [-97.92, 39.87], // center of US
     pitch: 0, // default pitch
     bearing: 0, // default bearing
-    zoom: 2, // default zoom
+    zoom: 3, // default zoom
     interactive: false // disable user interactivity
   };
 
@@ -42,11 +23,6 @@
     options: mapOptions,
     token: mapAccessToken
   };
-
-  // var leaflet = {
-  //   mapOpts: mapOpts,
-  //   tilesUrl: tilesUrl
-  // };
 
   var config = {
     searchData: 'bv_searchData',
@@ -60,7 +36,6 @@
 
   window.bvRPL = {
     config: config,
-    // leaflet: leaflet,
     mapbox: mapbox
   };
 
--- SOT-1272/PHONE_FLOW/js/map_animation.js
+++ SOT-1272/PHONE_FLOW/js/map_animation.js
@@ -1,123 +1,17 @@
-// (function ($, L, AnimationFrame) {
 (function ($) {
   var map;
-
-  // var circleOpts = {
-  //   color: '#b10810',
-  //   opacity: 1,
-  //   fillColor: '#b10810',
-  //   fillOpacity: 0.25,
-  //   weight: 2,
-  //   size: 0.5,
-  //   clickable: false,
-  // };
-
-  // var circle = L.circleMarker([38.505191, -97.734375], circleOpts);
-
-  // var resetCircleRadius = function () {
-  //   circleOpts.radius = ($(window).width() * 0.40) / 2;
-  //   circle.setRadius(circleOpts.radius);
-  // };
-  //
-  // resetCircleRadius();
-  //
-  // var circleRadius = circleOpts.radius;
-  // circle.setRadius(circleRadius);
-  // var currZoomLevel = 5;
-  // var circleMinRadius = 10;
-  // var circleStep = 2.5;
-  // @TODO: get actual phone location
-  var targetLocation = [-73.97, 40.73];
-
-  // var animateCircleRadius = function () {
-  //   if (currCircleRadius < circleMinRadius) {
-  //     window.cancelAnimationFrame(animateCircleRadiusId);
-  //     circle.setRadius(currCircleRadius);
-  //     map.setView(targetLocation, currZoomLevel, {animate: true});
-  //     nextTask();
-  //   } else {
-  //     currCircleRadius -= circleStep;
-  //     circle.setRadius(currCircleRadius);
-  //     animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
-  //   }
-  // };
-  //
-  // var runCircleAnimation = function (waitTime) {
-  //   circle.setRadius(circleOpts.radius);
-  //   window.setTimeout(function () {
-  //     currCircleRadius = circleOpts.radius;
-  //     animateCircleRadiusId = window.requestAnimationFrame(animateCircleRadius);
-  //   }, waitTime);
-  // };
-  //
-  // var transitionDuration = 500;
-
-  // var tasks = [
-  //   function () {
-  //     currZoomLevel = 7;
-  //     runCircleAnimation(transitionDuration);
-  //   },
-  //   function () {
-  //     currZoomLevel = 9;
-  //     runCircleAnimation(transitionDuration);
-  //   },
-  //   function () {
-  //     currZoomLevel = 12;
-  //     runCircleAnimation(transitionDuration);
-  //   }
-  // ];
-
+  var targetLocation = [];
   var finishedCb = function () {};
-
-  // var onCompletion = function () {
-  //   circle.setRadius(circleOpts.radius);
-  //   window.setTimeout(function () {
-  //     map.removeLayer(circle);
-  //     finishedCb();
-  //   }, 1200);
-  // };
-
-  // var currTask = -1;
-  // var maxTasks = tasks.length - 1;
-
-  // var nextTask = function () {
-  //   currTask += 1;
-  //   if (currTask <= maxTasks) {
-  //   tasks[currTask]();
-  //   } else {
-  //     onCompletion();
-  //   }
-  // };
-
-  // var phoneLocation = {
-  //   center: [-73.97, 40.73],
-  //   pitch: 60,
-  //   bearing: 15,
-  //   zoom: 12
-  // };
-
-  // var flyToLocation = function (map, location) {
-  //     map.flyTo({
-  //         center: location, // location of camera after animation ends
-  //         zoom: 12, // zoom of map after animation ends
-  //         pitch: 60, // pitch of map after animtion ends
-  //         bearing: 15, // bearing of map after animation ends
-  //         speed: 0.25, // speed of fly animation
-  //         curve: 0.75 // speed of zoom animation
-  //     });
-  // };
 
   var run = function (mapInst, location, delay, cb) {
     map = mapInst;
     finishedCb = cb;
     $(".initial_modal").fadeOut();
-    var $locating = $('.locating');
-    $locating.show();
+
     window.setTimeout(function () {
       $('.modal-overlay').fadeOut();
-      $locating.hide();
       targetLocation = location;
-      // map.panTo(targetLocation);
+
       map.flyTo({
           center: targetLocation, // location of camera after animation ends
           zoom: 12, // zoom of map after animation ends
@@ -126,9 +20,11 @@
           speed: 0.25, // speed of fly animation
           curve: 0.75 // speed of zoom animation
       });
-      // circle.setLatLng(targetLocation);
-      // circle.addTo(map);
-      nextTask();
+
+      window.setTimeout(function () {
+        finishedCb();
+      }, 15000); // 15s until next modal is shown
+
     }, delay);
   };
 
@@ -136,5 +32,4 @@
     run: run
   };
 
-// }(jQuery, L, AnimationFrame));
 }(jQuery));
