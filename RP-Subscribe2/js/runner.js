4e9de6765d219bf3de4fa4401daf67142218a655
Mikey Harris <mikey.harris89@gmail.com>
2017-06-26 20:21:15 -0400

    added most of desktop styles for subscribe 2

--- RP-Subscribe2/js/runner.js
+++ RP-Subscribe2/js/runner.js
@@ -1,6 +1,157 @@
 $(function() {
+  INPUT_OPTIONS = {
+    all : {
+
+    },
+    phone: {
+
+    }
+  };
+  /* Initialize cvv popover for Bootstrap 3 */
+  $('.cvv-img').popover({
+      container: 'body',
+      trigger: 'hover focus',
+      placement:'auto',
+      title:'<p><strong>What is a Security Code?</strong></p>',
+      html: true,
+      content: function () {
+          return '<p><strong>Visa, MasterCard, and Discover</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-visa.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Back of Card</strong><br>Three digits located on the right of the signature strip.</small></p></div></div><p><strong>American Express</strong></p><div class="row"><div class="col-xs-6"><img class="img-responsive" src="//manaron.s3.amazonaws.com/srg/hompage/web/img/cc-amex.png"></div><div class="col-xs-6 pop-text"><p><small><strong>Front of Card</strong><br>Four digits located on either the left or right side.</small></p></div>';
+      }
+  });
+  /* Initialize security popover for Bootstrap 3 */
+  $('#secure-lock').popover({
+      container: 'body',
+      trigger: 'hover focus',
+      placement:'auto',
+      html: true,
+      content: function () {
+          return '<p><span class="glyphicon glyphicon-lock"></span> BeenVerified deploys the latest and greatest strategies, including Secure 256-bit SSL technology, to keep your personal information and payment data safe from unauthorized 3rd parties.</p>';
+      }
+  });
+  $('#secure-text').on('hover' , function () {
+    $('#secure-lock').popover('toggle');
+  });
+
+  // Update terms & conditions based on selected plan
+  var updatePlanLegal = function ($selectionSelected) {
+
+    // var $planPrice = $selectionSelected.find(".plan-price");
+    //
+    // var totalPrice = $planPrice.data('plan-price'),
+    //     type = $planPrice.data('plan-type'),
+    //     monthlyPrice = $planPrice.data('monthly-price'),
+    //     termLength = $planPrice.data('term-length');
+    //
+    //
+    // var $priceHTML = $('#legal-price'),
+    //     $typeHTML = $('#legal-type'),
+    //     $summaryTermHTML = $('#summary-term'),
+    //     $summaryMonthPriceHTML = $('#summary-month-price'),
+    //     $summaryTotalPriceHTML = $('.summary-total-price');
+    //
+    // $priceHTML.html(totalPrice);
+    // $summaryTotalPriceHTML.html(totalPrice);
+    // $summaryMonthPriceHTML.html(monthlyPrice);
+    // $typeHTML.html(type);
+    // $summaryTermHTML.html(termLength);
+    //
+    // $selectionWrap.removeClass('selection-selected');
+    // $selectionSelected.addClass('selection-selected');
+  };
+
+  // Secure Credit Card lock animation
+  var $secureLockIcon = $('#secure-lock'),
+      $ccInput = $('#credit_card_card_number'),
+      $secureText = $('#secure-text');
+
+      // $secureText.hide();
+
+  $ccInput.on('focus' , function () {
+      $secureLockIcon.addClass('secure-this');
+      $secureText.fadeIn();
+  });
+
+  $ccInput.on('change blur' , function () {
+    var ccInputLength = $ccInput.val().length;
+
+    if (!ccInputLength) {
+      $secureText.hide();
+      $secureLockIcon.removeClass('secure-this');
+    }
+  });
+
+  // Clicking Credit Card logo will focus on input field
+  var $ccAccept = $('.cc-accept');
+
+  $ccAccept.on('click' , function () {
+    $ccInput.focus();
+  });
+
+  var verifySeal = function () {
+    $(".verify-seal").on("click", function () {
+      var left  = ($(window).width()/2)-(900/2),
+          top   = ($(window).height()/2)-(600/2),
+          popup = window.open ("https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&dn=www.beenverified.com&lang=en", "popup", "width=900, height=600, top="+top+", left="+left);
+    });
+  };
+
+  //added this fix to prevent freeze on page when paypal popup is exed out before loading is finished
+
+  $(document).on('DOMNodeRemoved', function(e){
+
+    if ($(e.target).is($('iframe'))){
+      if ($(e.target)[0].name.includes("lightbox_container")){
+
+        paypal.checkout.closeFlow();
+        $('#spinner').hide();
+        $('#create_button').removeAttr('disabled');
+      }
+    }
+  });
+
+  var getQueryArgs = function () {
+    var query = window.location.search.substring(1); // drop '?' char
+    if (!query) {
+      return false;
+    }
+    var args = _
+    .chain(query.split('&'))
+    .map(function(params) {
+      var p = params.split('=');
+      var key = p[0];
+      var val = window.decodeURIComponent(p[1]);
+      val = val.replace(/\/+$/, ""); // clean up trailing slash
+      return [key, val];
+    })
+    .object()
+    .value();
+
+    if (args.state === 'All') {
+      args.state = '';
+    }
+
+    return args;
+  };
+
+  var setHiddenInput = function(plan) {
+    $('.plan_name_radio').prop('checked', false);
+    if (plan === 'phone') {
+      $('#phone_plan').prop('checked', true);
+    } else {
+      $('#all_3_plan').prop('checked', true);
+    }
+  };
 
   var initialize = function() {
+    var query = getQueryArgs();
+    verifySeal();
+
+    if (query.plan){
+      // updatePlanLegal(query.plan);
+      setHiddenInput(query.plan);
+    } else {
+      window.location.href = $('body').data('previous-page');
+    }
 
   };
 
