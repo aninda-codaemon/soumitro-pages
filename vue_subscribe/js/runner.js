38f41196f80483582b5042a9b75b4fc1f03f0221
Mikey Harris <mikey.harris89@gmail.com>
2017-09-07 13:32:33 -0400

    Finished front end validations for real this time

--- vue_subscribe/js/runner.js
+++ vue_subscribe/js/runner.js
@@ -93,15 +93,34 @@
 
   !function(a){a.checkCC=function(a){String.prototype.startsWith=function(a){return this.match("^"+a)==a},Array.prototype.has=function(a,b){for(var c=0;c<this.length;c++)if(this[c]==a)return b?c:!0;return!1},a=a.replace(/[^0-9]/g,"");for(var b=[],c=0,d=0,e=a;0!==e;)b[c]=e%10,e-=b[c],e/=10,c++,d++;if(13>d)return"invalid";var f="invalid";if(a.startsWith("5")){if(16!=d)return"invalid";f=1}else if(a.startsWith("4")){if(16!=d&&13!=d)return"invalid";f=2}else if(a.startsWith("34")||a.startsWith("37")){if(15!=d)return"invalid";f=3}else if(a.startsWith("36")||a.startsWith("38")||a.startsWith("300")||a.startsWith("301")||a.startsWith("302")||a.startsWith("303")||a.startsWith("304")||a.startsWith("305")){if(14!=d)return"invalid";f=4}else if(a.startsWith("6011")){if(15!=d&&16!=d)return"invalid";f=5}else{if(a.startsWith("2014")||a.startsWith("2149"))return 15!=d&&16!=d?"invalid":6;if(a.startsWith("3")){if(16!=d)return"invalid";f=7}else{if(!a.startsWith("2131")&&!a.startsWith("1800"))return"invalid";if(15!=d)return"invalid";f=7}}var h,g=0;for(h=1;d>h;h+=2){var i=2*b[h];g+=i%10,g+=(i-i%10)/10}for(h=0;d>h;h+=2)g+=b[h];return 0!==g%10?"invalid":(return_vals={"-1":"invalid",1:"master",2:"Visa",3:"american_express",4:"invalid",5:"Discover",6:"invalid",7:"invalid"},return_vals[""+f])}}(this);
 
+  var ccChecked = function(ccField) {
+    // vuelidate's provided function requiredIf doesn't override other validators, so im adding the
+    // check to see if ccfields is selected, if not this field will automatically be valid
+    if (ccField) {
+      return true
+    }
+
+    return false;
+  }
   var validCC = function(ccNum) {
+
+    if (!ccChecked(this.ccFields)) {
+      return true;
+    }
+
     if (checkCC(ccNum) === 'invalid') {
         return false;
       } else {
         return true;
       }
   }
-  var validCVV = function (cvv, ccNum) {
-    var valid = true, cardType = checkCC(ccNum);
+  var validCVV = function(cvv) {
+
+    if (!ccChecked(this.ccFields)) {
+      return true;
+    }
+
+    var valid = true, cardType = checkCC(this.ccNum);
     if (cardType === "american_express") {
       valid = cvv && cvv.length === 4;
     } else {
@@ -110,7 +129,15 @@
     return valid;
   };
 
-  var validMonthYear = function (m,y) {
+  var validMonthYear = function () {
+
+    if (!ccChecked(this.ccFields)) {
+      return true;
+    }
+
+    m = this.exp_month,
+    y = this.exp_year;
+
     var today = new Date(),
       year = today.getFullYear(),
       month = today.getMonth() + 1;
@@ -128,24 +155,30 @@
     return isValid;
   };
 
-  var ccFieldsValidators = function(validator, ccFields){
-    debugger
-    if (!ccFields) {
-      return true
+  var validZip = function(value) {
+
+    if (!ccChecked(this.ccFields)) {
+      return true;
+    }
+
+    if (value.length < 5) {
+      return false;
     } else {
-      return false
+      return true;
     }
   }
 
   Vue.use(window.vuelidate.default);
   var required = window.validators.required,
       email = window.validators.email,
-      requiredIf = window.validators.requiredIf,
-      minLength = window.validators.minLength,
-      maxLength = window.validators.maxLength,
-      numeric = window.validators.numeric;
+      numeric = window.validators.numeric,
+      withParams = window.vuelidate.withParams;
 
-  window.validators.checkCC= checkCC;
+  window.validators.validMonthYear = withParams({type: 'validMonthYear'}, function(value){validMonthYear});
+  window.validators.validCVV = withParams({type: 'validCVV'}, function(value){validCVV(value)});
+  window.validators.validCC = withParams({type: 'validCC'}, function(value){validCC(value)});
+  window.validators.validZip = withParams({type: 'validZip'}, function(value){validZip(value)});
+
   var propertyInfo = new Vue({
     el: '#wrapper',
     data: applicationState,
@@ -172,10 +205,10 @@
 
       submitHandler: function(e){
         e.preventDefault();
-        debugger
+        // debugger
         if (this.$v.$invalid) {
           this.$v.$touch();
-          //shouldn't use jquery here so should fix this scrolling
+          //shouldn't use jquery here so should fix this scrolling eventually
           var offset = $('#sub-step-2').offset();
           if (offset) {
             window.scrollTo(0, offset.top);
@@ -207,30 +240,25 @@
         email
       },
       ccNum: {
-        requiredIf: requiredIf(function(){ return this.ccFields; }),
-        validCC: function(){ return ccFieldsValidators(validCC, this.ccFields, this.ccNum)},
+        validCC,
         numeric
       },
       cvv: {
-        requiredIf: requiredIf(function(){ return this.ccField}),
-        validCVV: function(){ return ccFieldsValidators(validCVV, this.ccFields, this.cvv)},
+        validCVV,
         numeric
       },
       exp_month: {
-        requiredIf: requiredIf(function(){ return this.ccFields; }),
-        validMonth: function() { return ccFieldsValidators(validMonthYear, this.ccFields, this.exp_month, this.exp_year)}
+        validMonthYear
       },
       exp_year: {
-        requiredIf: requiredIf(function(){ return this.ccFields; }),
-        validYear: function() { return ccFieldsValidators(validMonthYear, this.ccFields, this.exp_month, this.exp_year)}
+        validMonthYear
       },
       address_zip: {
-        requiredIf: requiredIf(function(){ return this.ccFields; }),
-        minLength: minLength(5),
+        validZip,
         numeric
       },
       tosChecked: {
-        required: required
+        required
       }
 
     }
