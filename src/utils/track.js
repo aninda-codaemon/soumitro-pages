da9727cb672067b8154a5d846a30c02136ff62c3
Bryan Azofeifa <bazofeifa@beenverified.com>
2017-10-14 19:15:01 -0600

    Refactor tracking

---
+++ src/utils/track.js
@@ -0,0 +1,30 @@
+const _get = 'lodash/get';
+
+const trackGA = (eventName, props) => {
+  if (typeof window.dataLayer !== 'undefined') {
+    var gaData = {
+      event: 'flowrida_visitor_event',
+      eventLabel: evtName,
+    };
+    if (props) {
+      gaData.visitorEventInfo = JSON.stringify(props)
+    }
+
+    window.dataLayer.push(gaData);
+  }
+};
+
+/**
+ * @param { eventName, props } args 
+ */
+const track = (...args) => {
+  const trackingFunctions = [
+    _.get(window, 'nolimit.track'),
+    _.get(window, 'heap.track'),
+    trackGA
+  ];
+
+  trackingFunctions.forEach(trackingFn => trackingFn && trackingFn(...args));
+};
+
+export { track };
