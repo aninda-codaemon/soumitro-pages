f6a79acf9b21d565a0e05491ee7148d09150d768
Bryan Azofeifa <bazofeifa@beenverified.com>
2017-10-14 19:26:02 -0600

    Remove tracking & localstorage check from the runner file

--- src/BeenVerified/People/Landing/Control/js/runner-fixpageload.js
+++ src/BeenVerified/People/Landing/Control/js/runner-fixpageload.js
@@ -1,45 +1,4 @@
 (function () {
-  // NoLimit/Heap init
-  var trackNL = function (evtName, props) {
-    if (typeof nolimit !== 'undefined' && nolimit.track) {
-      if (props) {
-        nolimit.track(evtName, props);
-      } else {
-        nolimit.track(evtName);
-      }
-    }
-    if (typeof heap !== 'undefined' && heap.track) {
-      if (props) {
-        heap.track(evtName, props);
-      } else {
-        heap.track(evtName);
-      }
-    }
-    if (typeof dataLayer !== 'undefined') {
-      var gaData;
-      if (props) {
-        gaData = { 
-          event: 'flowrida_visitor_event',
-          eventLabel: evtName,
-          visitorEventInfo: JSON.stringify(props) 
-        };
-        dataLayer.push(gaData);
-      } else {
-        gaData = { 
-          event: 'flowrida_visitor_event',
-          eventLabel: evtName
-        };
-        dataLayer.push(gaData);
-      }
-    }
-  };
-
-  // Test for Safari private browsing mode
-  try {
-    localStorage.test = 2;
-  } catch (e) {
-    trackNL("Safari Private Browsing");
-  }
 
   var BrowserDetect = {
     init: function () {
