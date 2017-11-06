1c8cb16ca2e12aeb8f78e8700ff4768a0423058e
Oliver Lai <oliverlai55@gmail.com>
2017-11-06 16:41:43 -0500

    Add default checkbox behavior and auto populate leadbox value

--- src/BeenVerified/People/BuildingReport/Variant-Email-Option/index.js
+++ src/BeenVerified/People/BuildingReport/Variant-Email-Option/index.js
@@ -1,4 +1,19 @@
 import { initialize } from '../Control/js/runner';
+import amplify from 'utils/amplifyStore';
 
 const shouldIncludeRelatives = true;
 initialize(shouldIncludeRelatives);
+
+// Default - Check emailbox
+$('#emailCheckbox').prop('checked', true);
+
+// Auto populate info form localStorage
+const leadData = amplify.store('leadData') || {};
+console.log(leadData);
+const firstName = leadData['account[first_name]'] || '';
+const lastName = leadData['account[last_name]'] || '';
+const email = leadData['user[email]'] || '';
+
+$('#lead_first_name').val(firstName);
+$('#lead_last_name').val(lastName);
+$('#lead_email').val(email);
