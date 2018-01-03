551b7b58255da820ea3a63adc3006d88a67b97f7
Bryan Azofeifa <bazofeifa@beenverified.com>
2018-01-03 09:21:07 -0600

    Add first tests!!

---
+++ test/utils/strings.test.js
@@ -0,0 +1,29 @@
+import {
+  capitalize,
+  properCaps,
+} from '../../src/utils/strings';
+
+describe('capitalize function', () => {
+  it('should capitalize a string', () => {
+    var text = 'hello';
+    var expectedText = 'Hello';
+
+    expect(capitalize(text)).toEqual(expectedText);
+  });
+
+  it('should return an empty string', () => {
+    var text = '';
+    var expectedText = '';
+
+    expect(capitalize(text)).toEqual(expectedText);
+  });
+});
+
+describe('properCaps function', () => {
+  it('should capitalize all the words within a string', () => {
+    var text = 'hello world';
+    var expectedText = 'Hello World';
+
+    expect(properCaps(text)).toEqual(expectedText);
+  });
+});
