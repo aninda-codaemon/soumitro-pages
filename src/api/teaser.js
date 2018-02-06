7e0f864f8e1f32b07049796d77d2ac1399246911
Bryan Azofeifa <bazofeifa@beenverified.com>
2018-02-06 08:01:52 -0600

    Remove the extra teaser info from the currentRecord

--- src/api/teaser.js
+++ src/api/teaser.js
@@ -82,8 +82,6 @@ const fixIncognitoMode = bvid => ({ teasers }) => {
      * Probably because those attributes are functions */
     currentRecord.fullName = parsedRecord.fullName();
     currentRecord.firstName = parsedRecord.firstName();
-    currentRecord.recordCount = parsedRecord.recordCount();
-    currentRecord.recordCountDisplay = parsedRecord.recordCountDisplay();
     amplify.store('currentRecord', currentRecord);
   }
 };
