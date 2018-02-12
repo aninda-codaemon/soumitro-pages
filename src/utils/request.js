0b6b6485612aeb96efb9aa56e17e28e9e3105762
Walter Chavarria <wchavarria03@gmail.com>
2018-02-12 11:44:17 -0600

    SOT-2353: Fixing results text issue and adding more time to fetch-jsonp before throws an error

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import fetchJsonp from 'fetch-jsonp';
 const get = (url, callbackFn = 'parseResults') => fetchJsonp(url, {
   mode: 'no-cors',
   jsonpCallbackFunction: callbackFn,
-  timeout: 10000,
+  timeout: 15000,
 }).then(response => response.json());
 
 export {
