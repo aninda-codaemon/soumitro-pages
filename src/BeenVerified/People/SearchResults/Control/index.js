61e41335c90b7285da228782f9d86918a133a136
Walter Chavarria <wchavarria03@gmail.com>
2018-05-25 22:06:48 -0600

    SOT-2708: Adding GA event

--- src/BeenVerified/People/SearchResults/Control/index.js
+++ src/BeenVerified/People/SearchResults/Control/index.js
@@ -1,3 +1,13 @@
+import { track } from 'utils/track';
 import { initialize } from './js/runner';
+
+// GA Tracking event by search result position
+$('#results').click((event) => {
+  let expectedTarget = $(event.target).closest('tr.results-row');
+  if (/results-row/.test(expectedTarget.attr('class'))) {
+    let position = expectedTarget.data('position');
+    track(`Search Result Position: ${++position}`);
+  }
+});
 
 initialize();
