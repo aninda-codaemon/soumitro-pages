07eaec3f81bfe2c4a93ad5e9ecb5447551213e35
Bryan Azofeifa <bazofeifa89@gmail.com>
2018-05-29 13:22:03 -0600

    SOT-2546 Persist the new query when the user go back to the Search Results Page

--- src/BeenVerified/People/SearchResults/Control/js/people-validator.js
+++ src/BeenVerified/People/SearchResults/Control/js/people-validator.js
@@ -2,6 +2,9 @@ import amplify from 'utils/amplifyStore';
 import peopleValidator from 'form-validators/people';
 import { cleanSearchValues } from 'utils/strings';
 
+const MODIFY_YOUR_SEARCH = 'search-form';
+const SKIP_SEG_FILE = '2597_263';
+
 const getSubmitHandler = onSubmit => function onSearchSubmit(form) {
   // close search menu if it's open (mobile)
   if ($('#nav-icon-toggle').hasClass('active')) {
@@ -13,11 +16,16 @@ const getSubmitHandler = onSubmit => function onSearchSubmit(form) {
     return prev;
   }, {});
   amplify.store('searchData', formVals);
+  if (form.id === MODIFY_YOUR_SEARCH && window.location.href.indexOf(SKIP_SEG_FILE) >= 0) {
+    const args = $(form).serialize();
+    const { origin, pathname } = window.location;
+    window.location = `${origin}${pathname}?${args}`;
+  }
   onSubmit(formVals);
 };
 
 const initializePeopleValidator = ({ onSubmit }) => {
-  var $searchForm = $('#search-form');
+  var $searchForm = $(`#${MODIFY_YOUR_SEARCH}`);
   var $refineForm = $('#refine-modal-form');
   var $noResultsForm = $('#no-results-form');
 
