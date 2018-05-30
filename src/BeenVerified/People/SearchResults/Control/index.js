f929ac3983fab953c98657466f4eba1b219bd72a
Walter Chavarria <wchavarria03@gmail.com>
2018-05-30 15:06:53 -0600

    SOT-2708: Adding temporary comment

--- src/BeenVerified/People/SearchResults/Control/index.js
+++ src/BeenVerified/People/SearchResults/Control/index.js
@@ -1,6 +1,7 @@
 import { track } from 'utils/track';
 import { initialize } from './js/runner';
 
+// Temporary Click Event - Should be removed when tests on SOT-2708 conclude
 // GA Tracking event by search result position
 $('#results').click((event) => {
   let expectedTarget = $(event.target).closest('tr.results-row');
