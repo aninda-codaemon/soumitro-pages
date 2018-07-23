c6809624e3a97a710782c89d421f86826cd2ee01
Aninda Kar <aninda.kar@outlook.com>
2018-07-23 23:21:42 +0530

    responsive tweaks and script streamline

--- src/components/building-report/Variant-sot-2638-bv-personalization/steps/bgSearchUseCases.js
+++ src/components/building-report/Variant-sot-2638-bv-personalization/steps/bgSearchUseCases.js
@@ -1,67 +1,48 @@
-/* eslint-disable */
 import Step from 'components/wizard/step';
 const $progressBar = $('#searching-progress-bar-database-bgsearch .progress-bar');
 const quotes = $('.info-list');
 const olList = $('.info-list-ol');
-const faceImgList = $('.contact-info-image');
-let $next = 1;
-let $current = 0;
-let $interval = 1950;
-let $fadeTime = 0;
-let $imgNum = 13;
 let quoteIndex = -1;
 let olListCounter = 0;
-let liCounter = 1;
+let liCounter = 0;
 let faceId;
 faceId = $(`.fim${liCounter}`);
-const showNextListQuote = (duration) => {
+
+ const showNextListQuote = (duration) => {
   quoteIndex++;
    if (quoteIndex < 7) {
-    quotes.eq(quoteIndex % quotes.length)
-     .fadeIn(1000, function() {
-        olList.eq(olListCounter).each(function() {
-           $(this).find('li').each(function(i) {
-              $(this).delay(2000 * i).show(120,function(){
-                 faceId.hide();
-                 liCounter++;
-                 faceId = $(`.fim${liCounter}`);
-                 faceId.show();
+      quotes.eq(quoteIndex % quotes.length)
+        .fadeIn(450, function() {
+          olList.eq(olListCounter).each(function() {
+            $(this).find('li').each(function(i) {
+                $(this).delay(1000 * i).show(110,function(){
+                  faceId.hide();
+                  liCounter++;
+                  faceId = $(`.fim${liCounter}`);
+                  faceId.show();
+                });
               });
-            });
-          olListCounter++;
-        }); 
-       })
-    .delay(duration / (quotes.length + 1))
-  .fadeOut(100, () => showNextListQuote(duration));
-  }
-};
-
-const loadFaceImg = (duration) => {
-  loopShowHideFaceImg();
-};
+            olListCounter++;
+          }); 
+        })
+      .delay(duration / (quotes.length + 1))
+      .fadeOut(100, () => showNextListQuote(duration));
+    }
+  };
 
 function onBgSearchUseCasesStart(stepCompleted) {
   var { duration } = this;
   var initialProgress = $($progressBar).animate({ width: '100%' }, { duration });
-   //$.when(loadFaceImg(duration)).then(showNextListQuote(duration));
    showNextListQuote(duration);
-   $.when(initialProgress).done(stepCompleted);
+  $.when(initialProgress).done(stepCompleted);
 }
-
-function loopShowHideFaceImg(){
-  faceImgList.eq($current).delay($interval).hide($fadeTime)
-  .end().eq($next).delay($interval).hide().show($fadeTime, loopShowHideFaceImg);
-
-  if($next < $imgNum-1){ $next++; } else { $next = 0;}
-  if($current < $imgNum-1){ $current++; } else { $current =0; }
-};
 
 function createComponent(options = {}) {
   const bgSearchUseCases = Object.assign({}, Step);
   let newConfig = Object.assign({
     title: 'Background Search',
     $elem: $('#gen-bgsearch-modal1'),
-    duration: 28,
+    duration: 20,
     onStart: onBgSearchUseCasesStart,
   }, options);
   bgSearchUseCases.init(newConfig);
@@ -69,4 +50,3 @@ function createComponent(options = {}) {
 }
 
 export default createComponent;
-
--- src/components/building-report/Variant-sot-2638-bv-personalization/steps/hiddenPastCriminal.js
+++ src/components/building-report/Variant-sot-2638-bv-personalization/steps/hiddenPastCriminal.js
@@ -6,27 +6,23 @@ const $part1 = $('.hidden-past-part1-speech-bub');
 const $part2 = $('.hidden-past-part2-speech-bub');
 
 const showNextQuote = (duration) => {
-  let quotesID;
   let interval = duration / ($quotes.length + 1);
   let maxSteps = 0;
   let part1Length = $part1.length;
 
   $part2.hide();
-  $quotes.hide();
-  $quotes.eq(maxSteps).fadeIn(1000);
+  $quotes.eq(maxSteps).css('visibility', 'visible').fadeIn(1000);
 
   const displayText = setInterval(() => {
     maxSteps++;
-    if (maxSteps == part1Length) {
+    if (maxSteps === part1Length) {
       clearInterval(displayText);
       $('.hidden-past-top-header').hide();
       $('div#hidden-past-section1').removeClass('icons-container');
       $part1.hide();
       $part2.show();
     }
-
-    if (maxSteps < part1Length) {
-    $quotes.eq(maxSteps).fadeIn(1000);}
+    $quotes.eq(maxSteps).css('visibility', 'visible').fadeIn(1000);
   }, interval);
 };
 
